694cb05808c9a4542d4f9956d00592b5c246836f
Wan Jong Kim <wanjongkim1@gmail.com>
2022-01-26 21:02:01 -0500

    Basic Starting Point for Bot

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
