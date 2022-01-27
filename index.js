7555b95e669dcbcde0e270b47fb9a32936343b08
Wan Jong Kim <wanjongkim1@gmail.com>
2022-01-26 22:57:31 -0500

    Registered first command

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 require('dotenv').config();
-
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-
-// Create a new client instance
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+client.commands = new Collection();
 
-// When the client is ready, run this code (only once)
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Login to Discord with your client's token
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 client.login(process.env.TOKEN);
